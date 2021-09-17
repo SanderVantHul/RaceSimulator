32749df57ca46611ed1c4487835c4a1eda15a379
SanderVantHul <sandervanthul98@gmail.com>
2021-09-17 11:38:19 +0200

    4.1 Track class
    Track class ConvertArrayToLinkedList method

---
+++ ControllerTest/Controller_Race_GetSectionData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Controller;
+using NUnit.Framework;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    class Controller_Race_GetSectionData
+    {
+        private Race _race;
+            
+        [SetUp]
+        public void Setup()
+        {
+            _race = new Race(null,null);
+        }
+
+        [Test]
+        public void GetSectionData_EmptyDictionary_ReturnNewSectionData()
+        {
+            
+        }
+        
+    }
+}
--- Model/Track.cs
+++ Model/Track.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Model
-{
+{ 
     public class Track
     {
         public string Name { get; }
@@ -12,7 +12,18 @@ namespace Model
         public Track(string name, SectionTypes[] sections)
         {
             Name = name;
-            Sections = new LinkedList<Section>();
+            Sections = ConvertArrayToLinkedList(sections);
+        }
+
+        public LinkedList<Section> ConvertArrayToLinkedList(SectionTypes[] sectionTypes)
+        {
+            var linkedList = new LinkedList<Section>();
+
+            foreach (var sectionType in sectionTypes)
+            {
+                linkedList.AddLast(new Section(sectionType));
+            }
+            return linkedList;
         }
     }
 }
